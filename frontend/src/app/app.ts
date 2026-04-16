a9740d277a13dc5e40f80bcf31263a3899ce321c
PABLO MARTÍNEZ GÁLVEZ <pmg943@inlumine.ual.es>
2026-04-16 19:41:36 +0200

    feat: add footer component

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/shared/header/header'; 
+import { FooterComponent } from './components/shared/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
